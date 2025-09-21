3b296f9b37629776dc409d32f096d53fa8710203
Brunno Ferreira <brunno.softwareengineer@gmail.com>
2025-09-21 15:39:41 -0300

    adicionando transicao de tela

---
+++ app-transicao-tela/componets/Senha.js
@@ -0,0 +1,20 @@
+import React ,{ useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet} from 'react-native';
+
+function senha ({ navigation }){
+
+    const senha=()
+
+    return (
+
+        <View>
+            <Text>Informe sua senha: </Text>
+        </View>
+
+    )
+
+    if
+
+}
+
+export default senha;
